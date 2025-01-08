2644367bfbc35e80e11490bccfafabb5ef6b6454
brundunn <brandon99@live.nl>
2025-01-08 08:59:44 +0100

    commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const paragraph = document.querySelector('p');
+//   paragraph.textContent = 'Build your own Cash Capsule!';
+// });
